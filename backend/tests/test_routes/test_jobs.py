4b4780d98e7a5f720672abe348ebde73320cd821
unknown <itexpleo@L-IRE-98GL7G3.ardianet.net>
2022-02-16 17:17:43 +0000

    Jobs, tests

---
+++ backend/tests/test_routes/test_jobs.py
@@ -0,0 +1,15 @@
+import json
+
+
+def test_create_job(client):
+    data={
+        "title":"SDE 1 Yahoo",
+        "company":"testhoo",
+        "company_url":"https://www.fdj.com",
+        "location":"USA,NY",
+        "description":"Testing",
+        "date_posted":"2020-07-20"
+        }
+
+    response=client.post("/job/create-job",json.dumps(data))
+    assert response.status_code==200
